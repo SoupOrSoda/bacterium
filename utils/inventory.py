a253156fbf1b676a8d046cd3ccbe55c409d70c54
SuperSoda <58730469+SoupOrSoda@users.noreply.github.com>
2021-03-31 11:48:27 +0100

    Update inventory.py

--- utils/inventory.py
+++ utils/inventory.py
@@ -1,4 +1,4 @@
-import json
+import pickle
 
 class inventoryManager():
     def __init__(self):
@@ -28,15 +28,20 @@ class inventoryManager():
                 self.items.remove(item)
             except Exception:
                 pass
+            
+    def importFromItemMan(itemMan):
+        self.items = itemMan.loi
+        print("Imported from loi.")
 
 class itemManager():
     def __init__(self):
         print("Created an itemManager")
         self.loi = []
         self.identifier = "<=====>"
+        self.listx = []
         
     def defineItem(self, name, iid):
-        temp = name+self.identifier+iid
+        temp = name+self.identifier+str(iid)
         if temp not in self.loi:
             self.loi.append(temp)
             return True
@@ -47,29 +52,39 @@ class itemManager():
         
         # This code is a mess
         # A big mess
+        # Lots of bad loops that make no sense leftover
+        # from the bad old system... but I guess that
+        # it works! I'll redo this code sometime soon.
+        # or not :)
         
         found = False
         indexValue = 0
         if name == None and not iid == None:
             while not found:
+                found = True
                 if indexValue > 1000:
                     return False
                 else:
                     indexValue =+ 1
                 x = self.loi
-                if x[1] == iid:
-                    return x[0] 
+                for b in x:
+                    b = b.split(self.identifier)
+                    if str(b[1]) == str(iid):
+                        return b[0]
             return False
         
         elif iid == None and not name == None:
             while not found:
+                found = True
                 if indexValue > 1000:
                     return False
                 else:
                     indexValue =+ 1
                 x = self.loi
-                if x[0] == name:
-                    return x[1] 
+                for b in x:
+                    b = b.split(self.identifier)
+                    if str(b[0]) == str(name):
+                        return b[1]
             return False
 
         # Here true means that you tried to get the name / iid, while having both!
@@ -78,5 +93,18 @@ class itemManager():
         return True
 
     def exportItems(self, file):
-        with open(str(file)+'.json', 'w') as f:
-            json.dump(self.loi, f)
+        with open(str(file)+'.pdata', 'wb') as f:
+            pickle.dump(self.loi, f)
+
+    def loadItems(self, file, listx):
+        with open(str(file)+'.pdata', 'rb') as f:
+            pickle.load(self.listx, f)
+
+itemMan = itemManager()
+itemMan.defineItem("Stone", 1)
+itemMan.defineItem("Cheese", 2)
+itemMan.defineItem("Rock", 3)
+print(itemMan.getItem(3))
+print(itemMan.getItem(2, "Cheese"))
+
+itemMan.exportItems("ooOOoLaaLaa")
